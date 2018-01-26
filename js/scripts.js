413bbeb7092f49fae9f579ce6822cfcc13bdc38f
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-01-26 11:43:07 -0800

    sepereate sections with divs in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
-function Pizza(size, topping) {
-  this.big = size;
+function Pizza(portion, topping) {
+  this.portion = portion;
   this.topping = topping; // maybe an array of toppings or string
 }
 
 Pizza.prototype.fullCost () {
-
+  return this.portion + ", " + this.topping + "." 
 }
+
+
+
+
+
+$(document).ready(function(){
+
+
+});
